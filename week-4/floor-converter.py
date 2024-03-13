b0162a5e3ad47914c36ae1c6df3e016501eb6525
Suyash <suyash.khandwe@gmail.com>
2024-03-12 23:27:05 -0700

    Adds a sample code for floor conversation from the lecture

---
+++ week-4/floor-converter.py
@@ -0,0 +1,4 @@
+# Convert Elevator Floors
+europefloor = input('Enter Europe floor: ')
+usfloor = int(europefloor) + 1
+print('Equivalent US floor is', usfloor)
--- week-4/user-inputs.py
+++ week-4/user-inputs.py
@@ -1,2 +1,2 @@
-inputvalue = input('Say something:\n')
+inputvalue = input('Say something: ')
 print('You said:', '\'' + inputvalue + '\'') # , adds a sapce
