e59e76df3b7785ab876e4c3ad6385d6524ebd58b
Suyash <suyash.khandwe@gmail.com>
2024-03-14 00:02:36 -0700

    Adds examples of loops

--- week-7/iterations.py
+++ week-7/iterations.py
@@ -5,3 +5,11 @@ while(X < 10):
     print(X)
     X = X + 1
 
+# Example of collections of integers
+for counter in [5,4,3,2,1]:
+    print(counter)
+print('BlastOff!!')
+
+# Example of collections of strings
+for friend in ["foo","bar","baz"]:
+    print("hey", friend)
