ebd8ffa04c3138c2941a48373df29f01da079313
Suyash <suyash.khandwe@gmail.com>
2024-03-13 23:59:21 -0700

    Demo of iterations

--- week-6/functions.py
+++ week-6/functions.py
@@ -1,6 +1,7 @@
 """Module for demo of functions."""
 
-# printit() : Ths will not work since the order of function definition matters and the function must be 'def'ined first before using.
+# printit() : Ths will not work since the order of function definition
+# matters and the function must be 'def'ined first before using.
 
 """Sample function to print something"""
 def printit():
---
+++ week-7/iterations.py
@@ -0,0 +1,7 @@
+"""Module for demo of loops/iterations."""
+
+X = 5
+while(X < 10):
+    print(X)
+    X = X + 1
+
