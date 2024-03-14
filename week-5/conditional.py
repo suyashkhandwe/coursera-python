29fcdaf01c060023e73933bab20813423a9376e9
Suyash <suyash.khandwe@gmail.com>
2024-03-13 22:14:15 -0700

    Sample basic conditional

--- week-5/conditional.py
+++ week-5/conditional.py
@@ -1,5 +1,6 @@
 x = 2
 if x < 10:
     print("less than 10")
+    # Indentation matters here which is typically 4 spaces and indents indicate where the current conditional starts and ends
 else:
     print("more than 2")
