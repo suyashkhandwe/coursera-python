a36f30822c44b163c71da57a7b8237fd35925a55
Suyash <suyash.khandwe@gmail.com>
2024-03-13 23:09:04 -0700

    First function

---
+++ week-6/functions.py
@@ -0,0 +1,6 @@
+# printit() : Ths will not work since the order of function definition matters and the function must be 'def'ined first before using.
+
+def printit():
+    print('hello from my function')
+
+printit()    
