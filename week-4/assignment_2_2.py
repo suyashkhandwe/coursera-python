970ec2b7a46a9ed7ebbac3a22ab346e163441606
Suyash <suyash.khandwe@gmail.com>
2024-03-13 23:42:40 -0700

    Adds assignment 4.6

--- week-4/assignment_2_2.py
+++ week-4/assignment_2_2.py
@@ -1,6 +1,6 @@
 """Module for assignment 2.2."""
 
-# Problem Statement - 
+# Problem Statement
 # 2.2 Write a program that uses input to prompt a user for their name and then welcomes them.
 # Note that input will pop up a dialog box.
 # Enter Sarah in the pop-up box when you are prompted so your output will match the desired output.
