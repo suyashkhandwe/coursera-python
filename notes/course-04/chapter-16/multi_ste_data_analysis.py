34926e6f28f82542f1527fc14a3dbc22cfb0eff7
Suyash <suyash.khandwe@gmail.com>
2024-05-15 00:06:16 -0700

    Adds notes for Chapter 16

---
+++ notes/course-04/chapter-16/multi_ste_data_analysis.py
@@ -0,0 +1,9 @@
+"""Module for Multi Step Data Analysis"""
+
+# We need a process which can pause and resume while gathering data.
+
+# Some professional tools include:
+# - Hadoop
+# - Spark
+# - AWS RedShift
+# - Pentaho
